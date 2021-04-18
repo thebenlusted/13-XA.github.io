8cc7cf1e75e223cd8c764e8a1fb4598150e18568
benlusted <lustedbenjamin@gmail.com>
2021-04-17 18:03:09 -0700

    changed site to landing page

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function openNav(){
-  var smallNav = document.getElementById("small-nav");
-    if(smallNav.style.display !== 'block'){
-      smallNav.style.display = 'block';
-    } else {
-      smallNav.style.display = 'none';
-    }
-  }
